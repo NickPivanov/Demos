a57504d6d35895a59154f312985f2d474d159734
Nick <diverkent01@gmail.com>
2021-05-17 10:44:01 +0200

    Add project to Git Repository

---
+++ ConsoleOnlineServiceReader/APIData/SunApiModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleOnlineServiceReader.APIData
+{
+    public class SunApiModel
+    {
+        public Sun Results { get; set; }
+    }
+
+    public class Sun
+    {
+        public DateTime Sunrise { get; set; }
+        public DateTime Sunset { get; set; }
+    }
+}
---
+++ ConsoleOnlineServiceReader/Program.cs
@@ -0,0 +1,13 @@
+﻿using ConsoleOnlineServiceReader.APIData;
+using System;
+
+
+Console.WriteLine("Hello World!");
+
+ClientInitializer.InitializeClient();
+
+var sunInfo = await SunApiProcessor.GetDataForKaliningradAsync();
+
+string textResult = $"Sunrise in Kaliningrad ABB office at {sunInfo.Sunrise.ToLocalTime().ToShortTimeString()}" +
+                $"\nSunset in Kaliningrad ABB office at {sunInfo.Sunset.ToLocalTime().ToShortTimeString()}";
+Console.WriteLine(textResult);
