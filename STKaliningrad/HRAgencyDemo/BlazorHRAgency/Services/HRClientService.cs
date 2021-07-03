96d9dcf7c326e6a72989f1195760f8efe1ae21bb
Nick <diverkent01@gmail.com>
2021-07-03 12:25:43 +0200

    HRAgency Demo

---
+++ STKaliningrad/HRAgencyDemo/BlazorHRAgency/Services/HRClientService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BlazorHRAgency.Services
+{
+    public class HRClientService
+    {
+        private IHttpClientFactory _httpClientFactory;
+        private HttpClient httpClient;
+        private hrClient client;
+        public hrClient HrClient { get; set; }
+        public HRClientService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+            httpClient = _httpClientFactory.CreateClient("hrAPI");
+            client = new hrClient("", httpClient);
+            HrClient = client;
+        }
+    }
+}
--- STKaliningrad/HRAgencyDemo/BlazorHRAgency/Startup.cs
+++ STKaliningrad/HRAgencyDemo/BlazorHRAgency/Startup.cs
@@ -47,6 +47,7 @@ namespace BlazorHRAgency
             services.AddSingleton<AppState>();
             services.AddTransient<Components.Details>();
             services.AddTransient<Components.Access>();
+            services.AddTransient<Services.HRClientService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
