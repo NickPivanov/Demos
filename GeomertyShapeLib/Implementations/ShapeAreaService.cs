82b41dfa4564ea1f42d6179670c9bfd311b1cbe8
Nikolay Pivanov <it3@tstarter.ru>
2024-09-09 14:08:42 +0200

    Added IShapeAreaService implementation

---
+++ GeomertyShapeLib/Implementations/ShapeAreaService.cs
@@ -0,0 +1,22 @@
+﻿using GeomertyShapeLib.Abstractions;
+using GeomertyShapeLib.Models;
+
+namespace GeomertyShapeLib.Implementations;
+
+public sealed class ShapeAreaService : IShapeAreaService
+{
+    public double GetShapeArea<TShape>(TShape shape) where TShape : ShapeBase
+    {
+        return shape.GetShapeArea();
+    }
+
+    public double GetCircleArea(double radius)
+    {
+        return new Circle(radius).GetShapeArea();
+    }
+
+    public double GetTriangleArea(double sideA, double sideB, double sideC)
+    {
+        return new Triangle(sideA, sideB, sideC).GetShapeArea();
+    }
+}
