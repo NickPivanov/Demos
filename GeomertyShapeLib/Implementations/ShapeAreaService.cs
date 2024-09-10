2ba73c675900b062c0465facf0a62f848a5ec73c
Nikolay Pivanov <it3@tstarter.ru>
2024-09-10 20:03:11 +0200

    Sql request

--- GeomertyShapeLib/Implementations/ShapeAreaService.cs
+++ GeomertyShapeLib/Implementations/ShapeAreaService.cs
@@ -21,7 +21,7 @@ public sealed class ShapeAreaService : IShapeAreaService
     public double GetTriangleArea(double legA, double legB, double hypotenuse)
     {
         if (legA <= 0 || legB <= 0 || hypotenuse <= 0)
-            throw new("Triangle should be greater then 0");
+            throw new("Triangle legs should be greater then 0");
 
         return new Triangle(legA, legB, hypotenuse).GetShapeArea();
     }
