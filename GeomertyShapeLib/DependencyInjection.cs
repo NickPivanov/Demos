fde55d1ffe9dc52f53d5de1b0eb24a66826a357f
Nikolay Pivanov <it3@tstarter.ru>
2024-09-10 09:00:25 +0200

    Add dependency injection extension

---
+++ GeomertyShapeLib/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using GeomertyShapeLib.Abstractions;
+using GeomertyShapeLib.Implementations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GeomertyShapeLib;
+
+public static class DependencyInjection
+{
+    public static void AddShapeAreaService(this IServiceCollection services)
+    {
+        services.AddTransient<IShapeAreaService, ShapeAreaService>();
+    }
+}
